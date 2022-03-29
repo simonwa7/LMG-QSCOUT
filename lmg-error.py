ccd4ada6fd4988c18eefde691c96bc9c1bd8d43b
simonwa7 <william.andrew.simon@gmail.com>
2022-03-29 16:15:08 -0400

    Cleaning up code

--- lmg-error.py
+++ lmg-error.py
@@ -10,36 +10,29 @@ from measurement import (
     create_clique4_circuit,
     get_measurements_for_clique,
 )
+import os
+import jaqalpaq
+from qscout.v1.std.ionsim import IonSimErrorModel
+import jaqalpaq.core.result
 
+jaqalpaq.core.result.ProbabilisticSubcircuit.CUTOFF_FAIL = 1e-4
+jaqalpaq.core.result.ProbabilisticSubcircuit.CUTOFF_WARN = 1e-4
+NUMBER_OF_SAMPLES = 10000
+RESULTS_DIR_NAME = "results/jaqalpaq/point/ionsim/"
 
-for NUMBER_OF_QUBITS in [1, 2, 3]:
-    NUMBER_OF_SAMPLES = 10000
-    PARAMETERS_MAP = {
-        1: M1_POINT_PARAMETER_VALUES,
-        2: M2_POINT_PARAMETER_VALUES,
-        3: M3_POINT_PARAMETER_VALUES,
-    }
-    RESULTS_DIR_NAME = "results/jaqalpaq/point/ionsim/"
+if not os.path.exists(RESULTS_DIR_NAME):
+    os.makedirs(RESULTS_DIR_NAME)
 
-    import os
-
-    if not os.path.exists(RESULTS_DIR_NAME):
-        os.makedirs(RESULTS_DIR_NAME)
-
-    import jaqalpaq
-
-    from qscout.v1.std.ionsim import IonSimErrorModel
-
-    import jaqalpaq.core.result
-
-    jaqalpaq.core.result.ProbabilisticSubcircuit.CUTOFF_FAIL = 1e-4
-    jaqalpaq.core.result.ProbabilisticSubcircuit.CUTOFF_WARN = 1e-4
+for number_of_qubits, circuit_parameters in zip(
+    [1, 2, 3],
+    [M1_POINT_PARAMETER_VALUES, M2_POINT_PARAMETER_VALUES, M3_POINT_PARAMETER_VALUES],
+):
 
     # Replace this noise model with interpygate noise model
-    # backend = noisy.SNLToy1(NUMBER_OF_QUBITS)
+    # backend = noisy.SNLToy1(number_of_qubits)
     # backend = None
     backend = IonSimErrorModel(
-        NUMBER_OF_QUBITS,
+        number_of_qubits,
         model="standard",
         params=["dpower12", "dfreq1", "dphase1", "dtime"],
         v0={"dpower12": 5e-4, "dfreq1": 5e3, "dphase1": 5e-2, "dtime": 5e-3},
@@ -47,9 +40,9 @@ for NUMBER_OF_QUBITS in [1, 2, 3]:
     )
 
     cliques_to_measure = [create_clique1_circuit, create_clique2_circuit]
-    if NUMBER_OF_QUBITS > 1:
+    if number_of_qubits > 1:
         cliques_to_measure.append(create_clique3_circuit)
-    if NUMBER_OF_QUBITS > 2:
+    if number_of_qubits > 2:
         cliques_to_measure.append(create_clique4_circuit)
 
     all_measurements = []
@@ -57,14 +50,14 @@ for NUMBER_OF_QUBITS in [1, 2, 3]:
         all_measurements.append(
             get_measurements_for_clique(
                 clique,
-                PARAMETERS_MAP[NUMBER_OF_QUBITS],
-                NUMBER_OF_QUBITS,
+                circuit_parameters,
+                number_of_qubits,
                 NUMBER_OF_SAMPLES,
                 backend=backend,
             )
         )
 
-    data_filename = RESULTS_DIR_NAME + "{}.txt".format(NUMBER_OF_QUBITS)
+    data_filename = RESULTS_DIR_NAME + "{}.txt".format(number_of_qubits)
     with open(data_filename, "w") as f:
         f.write(str(all_measurements))
     f.close()
--- lmg-grid.py
+++ lmg-grid.py
@@ -7,57 +7,54 @@ from measurement import (
     get_measurements_for_clique,
 )
 
-
-NUMBER_OF_QUBITS = 1
-NUMBER_OF_SAMPLES = 2 ** 13
-PARAMETERS_MAP = {1: M1_GRID_PARAMETERS, 2: M2_GRID_PARAMETERS, 3: M3_GRID_PARAMETERS}
-RESULTS_DIR_NAME = "results/jaqalpaq/grid/ionsim/{}/".format(NUMBER_OF_QUBITS)
-
 import os
-
-if not os.path.exists(RESULTS_DIR_NAME):
-    os.makedirs(RESULTS_DIR_NAME)
-
 import jaqalpaq
-
 from qscout.v1.std.ionsim import IonSimErrorModel
-
 import jaqalpaq.core.result
 
 jaqalpaq.core.result.ProbabilisticSubcircuit.CUTOFF_FAIL = 1e-4
 jaqalpaq.core.result.ProbabilisticSubcircuit.CUTOFF_WARN = 1e-4
+NUMBER_OF_SAMPLES = 2 ** 13
 
-# backend=None
-# backend = noisy.SNLToy1(NUMBER_OF_QUBITS)
-backend = IonSimErrorModel(
-    NUMBER_OF_QUBITS,
-    model="standard",
-    params=["dpower12", "dfreq1", "dphase1", "dtime"],
-    v0={"dpower12": 5e-4, "dfreq1": 5e3, "dphase1": 5e-2, "dtime": 5e-3},
-    sigmas={"dpower12": 5e-4, "dfreq1": 5e3, "dphase1": 5e-2, "dtime": 5e-3},
-)
+for number_of_qubits, grid_parameters in zip(
+    [1, 2, 3],
+    [M1_GRID_PARAMETERS, M2_GRID_PARAMETERS, M3_GRID_PARAMETERS],
+):
+    RESULTS_DIR_NAME = "results/jaqalpaq/grid/ionsim/{}/".format(number_of_qubits)
 
+    if not os.path.exists(RESULTS_DIR_NAME):
+        os.makedirs(RESULTS_DIR_NAME)
 
-for i, circuit_parameters in enumerate(PARAMETERS_MAP[NUMBER_OF_QUBITS]):
-    cliques_to_measure = [create_clique1_circuit, create_clique2_circuit]
-    if NUMBER_OF_QUBITS > 1:
-        cliques_to_measure.append(create_clique3_circuit)
-    if NUMBER_OF_QUBITS > 2:
-        cliques_to_measure.append(create_clique4_circuit)
+    # Choose noise model
+    # backend=None
+    backend = IonSimErrorModel(
+        number_of_qubits,
+        model="standard",
+        params=["dpower12", "dfreq1", "dphase1", "dtime"],
+        v0={"dpower12": 5e-4, "dfreq1": 5e3, "dphase1": 5e-2, "dtime": 5e-3},
+        sigmas={"dpower12": 5e-4, "dfreq1": 5e3, "dphase1": 5e-2, "dtime": 5e-3},
+    )
 
-    all_measurements = []
-    for clique in cliques_to_measure:
-        all_measurements.append(
-            get_measurements_for_clique(
-                clique,
-                circuit_parameters,
-                NUMBER_OF_QUBITS,
-                NUMBER_OF_SAMPLES,
-                backend=backend,
+    for i, circuit_parameters in enumerate(grid_parameters):
+        cliques_to_measure = [create_clique1_circuit, create_clique2_circuit]
+        if number_of_qubits > 1:
+            cliques_to_measure.append(create_clique3_circuit)
+        if number_of_qubits > 2:
+            cliques_to_measure.append(create_clique4_circuit)
+
+        all_measurements = []
+        for clique in cliques_to_measure:
+            all_measurements.append(
+                get_measurements_for_clique(
+                    clique,
+                    circuit_parameters,
+                    number_of_qubits,
+                    NUMBER_OF_SAMPLES,
+                    backend=backend,
+                )
             )
-        )
 
-    data_filename = RESULTS_DIR_NAME + "{}.txt".format(i + 1)
-    with open(data_filename, "w") as f:
-        f.write(str(all_measurements))
-    f.close()
+        data_filename = RESULTS_DIR_NAME + "{}.txt".format(i + 1)
+        with open(data_filename, "w") as f:
+            f.write(str(all_measurements))
+        f.close()
